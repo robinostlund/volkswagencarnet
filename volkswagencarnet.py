df4a473f3f9161d06fc73a517bb049d82bff1ea1
Robin Östlund <me@robinostlund.name>
2020-01-25 11:54:51 +0100

    updates

--- volkswagencarnet.py
+++ volkswagencarnet.py
@@ -18,7 +18,7 @@ from utilities import find_path, is_valid_path
 
 version_info >= (3, 0) or exit('Python 3 required')
 
-__version__ = '4.1.4'
+__version__ = '4.1.5'
 
 _LOGGER = logging.getLogger(__name__)
 
