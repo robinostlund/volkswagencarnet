a1694e4114a68e907aea7bbac7b9a8d93eecbf2e
Robin Östlund <me@robinostlund.name>
2020-06-15 21:30:59 +0200

    new version

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='volkswagencarnet',
     # version='4.1.31',
@@ -8,6 +14,8 @@ setup(
     author_email='me@robinostlund.name',
     url='https://github.com/robinostlund/volkswagencarnet',
     # download_url='https://github.com/robinostlund/volkswagencarnet/archive/4.1.31.tar.gz',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     py_modules=[
         "volkswagencarnet",
         "dashboard",
@@ -19,5 +27,7 @@ setup(
         'requests',
         'lxml',
         'beautifulsoup4'
-    ]
+    ],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
 )
