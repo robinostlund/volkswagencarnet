f83bc95460043f0353e419733ad47672a80c59af
Oliver Rahner <o.rahner@dke-data.com>
2023-12-08 11:06:47 +0100

    fix 204 handling

--- volkswagencarnet/vw_connection.py
+++ volkswagencarnet/vw_connection.py
@@ -402,7 +402,7 @@ class Connection:
 
             try:
                 if response.status == 204:
-                    res = {"status_code": response.status, "body": response.text}
+                    res = {"status_code": response.status}
                 elif response.status >= 200 or response.status <= 300:
                     res = await response.json(loads=json_loads)
                 else:
